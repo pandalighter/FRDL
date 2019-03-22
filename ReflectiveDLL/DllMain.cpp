7637b559740a80d9db84a58ecf9efd029074d4e3
pandalighter <47146831+pandalighter@users.noreply.github.com>
2019-03-22 07:09:28 -0700

    Add files via upload

---
+++ ReflectiveDLL/DllMain.cpp
@@ -0,0 +1,31 @@
+#include"FRDL.h"
+
+
+
+#define DLLEXPORT  extern"C" __declspec( dllexport ) 
+
+
+
+
+extern HINSTANCE hAppInstance;
+//===============================================================================================//
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
+{
+	BOOL bReturnValue = TRUE;
+	switch (dwReason)
+	{
+	case DLL_QUERY_HMODULE:
+		if (lpReserved != NULL)
+			*(HMODULE *)lpReserved = hAppInstance;
+		break;
+	case DLL_PROCESS_ATTACH:
+		hAppInstance = hinstDLL;
+		MessageBoxA(NULL, "Hello from DllMain!", "Reflective Dll Injection", MB_OK);
+		break;
+	case DLL_PROCESS_DETACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return bReturnValue;
+}
